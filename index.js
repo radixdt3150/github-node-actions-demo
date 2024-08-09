ef18f0a7ccf31c72dac181e7d7c1f1cf8914b96d
radixdt3150 <mayur.upadhyay@radixweb.com>
2024-08-09 16:38:16 +0530

    chore: Minor refactoring

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-import express from "express";
-import bodyParser from "body-parser";
-
+const express = require("express")
+const bodyParser = require("body-parser")
 
 const app = express();
 const port = 5000;
 
 app.use(bodyParser.json())
 
-app.get('/', (res) => {
+app.get('/', (_, res) => {
     res.send("Root route")
 })
 
